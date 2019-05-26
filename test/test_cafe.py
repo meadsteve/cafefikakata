9fbca2f3733b088e4f11363ef8d21c9e6a1c1ec5
steve <meadsteve@gmail.com>
2019-05-26 12:27:28 +0200

    (requirement 1) parametrize the get_price test

--- test/test_cafe.py
+++ test/test_cafe.py
@@ -1,11 +1,21 @@
+import pytest
+
 from cafe.money import Krona
-from cafe.cafe import Cafe
+from cafe.cafe import Cafe, Item
 
 
 def test_money_can_be_equal():
     assert Krona(5) == Krona(5)
 
 
-def test_cafe_have_prices():
+expected_prices = [
+    ("coffee", Krona(5)),
+    ("fancy coffee", Krona(8)),
+    ("kanelbulle", Krona(10))
+]
+
+
+@pytest.mark.parametrize("item,expected_price", expected_prices)
+def test_cafe_have_prices(item: Item, expected_price: Krona):
     cafe = Cafe()
-    assert cafe.get_prices("coffee") == Krona(5)
+    assert cafe.get_prices(item) == expected_price
