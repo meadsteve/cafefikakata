a17888c29f67abd1c792742ac5f2ae4eefdf0970
steve <meadsteve@gmail.com>
2019-05-26 13:04:05 +0200

    (requirement 3) refactor cafe out of test cases

--- test/test_cafe.py
+++ test/test_cafe.py
@@ -3,6 +3,8 @@ import pytest
 from cafe.money import Krona
 from cafe.cafe import Cafe, Item, Order
 
+cafe = Cafe()
+
 expected_prices = [
     ("coffee", Krona(5)),
     ("fancy coffee", Krona(8)),
@@ -12,12 +14,10 @@ expected_prices = [
 
 @pytest.mark.parametrize("item,expected_price", expected_prices)
 def test_cafe_have_prices(item: Item, expected_price: Krona):
-    cafe = Cafe()
     assert cafe.get_price(item) == expected_price
 
 
 def test_a_cafe_can_give_you_a_receipt_for_the_order():
-    cafe = Cafe()
     order = Order(["coffee", "coffee", "kanelbulle"])
     receipt = cafe.get_receipt(order)
     assert receipt.total == Krona(20)
@@ -32,5 +32,4 @@ expected_deals = [
 
 @pytest.mark.parametrize("order,expected_price", expected_deals)
 def test_a_cafe_can_run_special_offers(order: Order, expected_price: Krona):
-    cafe = Cafe()
     assert cafe.get_receipt(order).total == expected_price
