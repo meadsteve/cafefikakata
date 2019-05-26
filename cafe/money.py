1c11904d4d9c43964b7c04a5e1171daa409c1c57
steve <meadsteve@gmail.com>
2019-05-26 14:23:21 +0200

    (requirement 7) Add BritishPoundsCurrency

--- cafe/money.py
+++ cafe/money.py
@@ -12,12 +12,17 @@ class Currency:
             return self
         if type(other) == type(self):
             return self.__class__(self.amount + other.amount)
+        raise ValueError("Cannot perform addition")
 
     def __mul__(self, other):
         return self.__class__(self.amount * other)
 
 
 class Krona(Currency):
+    pass
+
+
+class BritishPounds(Currency):
     pass
 
 
--- test/test_money.py
+++ test/test_money.py
@@ -1,9 +1,20 @@
+import pytest
+
 from cafe import money
-from cafe.money import Krona
+from cafe.money import Krona, BritishPounds
 
 
 def test_money_can_be_equal():
     assert Krona(5) == Krona(5)
+
+
+def test_different_currencies_are_not_equal():
+    assert Krona(5) != BritishPounds(5)
+
+
+def test_different_currencies_cannot_be_added():
+    with pytest.raises(ValueError):
+        assert Krona(5) + BritishPounds(5)
 
 
 def test_money_can_be_totalled():
